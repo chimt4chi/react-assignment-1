9bfb3945b00b1ccc5514db632de9aa3a3850c86f
rohit singh <rs9990271.edu@gmail.com>
2025-09-19 18:31:44 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Greetings from "./components/Greetings";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [warning, setWarning] = useState("");
+
+  const handleCountDec = () => {
+    if (count <= 0) {
+      setWarning("Counter cannot be negative!");
+      return;
+    }
+    setCount(count - 1);
+    setWarning("");
+  };
+
+  const handleCountInc = () => {
+    setCount(count + 1);
+    setWarning("");
+  };
 
   return (
     <>
+      <h1>Q1. Counter App</h1>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <button onClick={handleCountDec}>-</button>
+        <p>{`count is ${count} `} </p>
+        <button onClick={handleCountInc}>+</button>
+        {/* {count < 0 ? <p>Counter cannot be negative!</p> : null} */}
+        {/* counter still gets updated */}
+        {warning && <p>{warning}</p>}
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Q2. User Greeting Component</h2>
+        <Greetings name={"Rohit"} />
+        <Greetings />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,5 @@
+function Greetings({ name = "Guest" }) {
+  return <div>Hello {name}!</div>;
+}
+
+export default Greetings;
